d027b7534e3bff3752737f1f22f5658bd9f35068
Harsheel Sharma <143731909+harshu1705@users.noreply.github.com>
2024-03-31 04:32:37 +0000

    data

---
+++ 01_basics/t1.js
@@ -0,0 +1,17 @@
+
+const accountId  = 144553
+let accountEMail = "sharmahasrheel@gmail.com"
+var accountPassword = "1234"
+accountCity = "Jaipur"
+let accountstate;         //undefined aayega as = nahi hein
+
+// accountid = 2 not allowed
+//  accountId  = 144553
+  accountEMail = "shajjgvvhv"
+accountCity = "mumbai"
+console.log(accountId);
+console.table([accountEMail,accountId,accountPassword,accountCity,accountstate])
+
+/*
+prefer not use to var a
+*/
