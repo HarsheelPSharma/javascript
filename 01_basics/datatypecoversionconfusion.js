1e0664e9b54f1610bae3ff95825af23ca371c57e
Harsheel Sharma <143731909+harshu1705@users.noreply.github.com>
2024-03-31 08:06:47 +0000

    deee

---
+++ 01_basics/datatypecoversionconfusion.js
@@ -0,0 +1,31 @@
+// let score = "TRUE"
+
+// console.log(typeof score)
+
+// let vauleInNumber = Number(score)
+// console.log(typeof vauleInNumber)
+// console.log(vauleInNumber)
+
+
+// // "33"=>33
+//"33abc"=>Not a number NaN
+//true ==>1;false==>0
+
+// let isLoggedIn = ""
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 =>>true; 0=> false
+//""=>false
+//"Hsrhekbnsb gsh "=>true
+
+let someNumber =  33
+let stringNumber = String(someNumber)
+console.log(stringNumber );
+console.log(typeof stringNumber );
+
+
+
+
+
+
