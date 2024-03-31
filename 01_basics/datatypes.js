05a1fe1fc99d15b3b1d89967a8d89dbd83385d57
Harsheel Sharma <143731909+harshu1705@users.noreply.github.com>
2024-03-31 04:31:54 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; //treat all code as newer version
+// alert(3+3) // it will show refference error as alert is not defineda as we are doin on node
+
+console.log(3-6)  //code readibility should be high
+let name ="Harshu"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+//number ==> 2 to power 53
+//bigint                      used in trading website
+// string must be used in double quotes as single quote acahe nahi lagte
+//boolean ==>true/false
+// null ==standalone value
+//undefined ==> abhi value assign nahi kiya
+
+//sybmol ==> unique in react
+
+console.log(typeof "state")
+
+
+
+// Object
+console.log(typeof undefined) //Undefined
+console.log(typeof null)  //Object
+
+
+
+
+
+
+ 
